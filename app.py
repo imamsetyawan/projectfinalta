df0eaa9436242bfe1e22076b5eac6870ba8761f4
imamsetyawan <111202012526@mhs.dinus.ac.id>
2026-06-28 12:49:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sns_custom_or_seaborn as sns
 import seaborn as sns
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
